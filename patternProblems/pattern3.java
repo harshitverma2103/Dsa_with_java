1aeb8e86ed1a4bc0a4ed5770091a9456222d640f
harshitverma2103 <harshitverma2103@gmail.com>
2023-12-24 21:29:28 +0530

    Number increasing reverse pyramid

---
+++ patternProblems/pattern3.java
@@ -0,0 +1,18 @@
+package patternProblems;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+
+    public static void pattern(int n) {
+        for (int i = n; i >= 1; i--) {
+            String result = "";
+            for (int j = 1; j <= i; j++) {
+                result += j;
+            }
+            System.out.println(result);
+        }
+        System.out.println();
+    }
+}
