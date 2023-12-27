3afa45e449f71ced64bd6be03df46fc5e71ec25a
harshitverma2103 <harshitverma2103@gmail.com>
2023-12-27 20:12:11 +0530

    number pattern

---
+++ patternProblems/pattern8.java
@@ -0,0 +1,17 @@
+package patternProblems;
+
+public class pattern8 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            String result = "";
+            for (int j = 1; j <= i; j++) {
+                result += i;
+            }
+            System.out.println(result);
+        }
+    }
+}
