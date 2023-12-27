db466bdca82dd36409099d7cfdec9259923c9dce
harshitverma2103 <harshitverma2103@gmail.com>
2023-12-27 21:24:49 +0530

    Number triangle pattern

---
+++ patternProblems/pattern10.java
@@ -0,0 +1,21 @@
+package patternProblems;
+
+public class pattern10 {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+
+    public static void pattern(int n) {
+        int i, j;
+        
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
