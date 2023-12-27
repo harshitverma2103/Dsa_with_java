150408658840756b108ba2449974b80b72dfb839
harshitverma2103 <harshitverma2103@gmail.com>
2023-12-27 14:12:30 +0530

    star triangle

---
+++ patternProblems/pattern7.java
@@ -0,0 +1,16 @@
+package patternProblems;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n) {
+        String result = "";
+        for (int i = n; i >= 1; i--) {
+            result += "*";
+            System.out.println(result);
+        }
+        n--;
+    }
+}
