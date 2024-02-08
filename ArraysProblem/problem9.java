ff3ce0a969dedf127b03a1ade33f8fc06fe3c964
harshitverma2103 <harshitverma2103@gmail.com>
2024-02-08 18:34:15 +0530

    plusOne

---
+++ ArraysProblem/problem9.java
@@ -0,0 +1,32 @@
+package ArraysProblem;
+
+import java.util.Arrays;
+
+public class problem9 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int[] result = plusOne(arr);
+        printArray(result);
+    }
+
+    public static int[] plusOne(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++;
+                return arr;
+            }
+            arr[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
